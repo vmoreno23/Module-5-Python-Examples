b980f83f27fe97e0b18079f89d36c04c20fe8a10
vmoreno23 <123032245+vmoreno23@users.noreply.github.com>
2024-02-16 20:27:43 -0600

    Update Lab 5 P5.py

--- Lab 5 P5.py
+++ Lab 5 P5.py
@@ -2,17 +2,18 @@ import turtle
 wn = turtle.Screen()
 drawing = turtle.Turtle()
 
-drawing.color("blue")
-drawing.fillcolor("blue")
+drawing.color("blue") #sets outline to blue
+drawing.fillcolor("blue") #sets fill color to blue 
 
-drawing.begin_fill()
-for i in [1,2]:
+drawing.begin_fill() #begins fill process 
+for i in [1,2]: #draws square portion
     drawing.forward(210)
     drawing.right(90)
     drawing.forward(210)
     drawing.right(90)
-drawing.end_fill()
+drawing.end_fill() #ends fill process
 
+#Draws top trangle portion
 drawing.fillcolor("red")
 drawing.begin_fill()
 drawing.color("red")
@@ -22,6 +23,7 @@ drawing.right(90)
 drawing.forward(148)
 drawing.end_fill()
 
+#Draws right side triangle portion
 drawing.fillcolor("red")
 drawing.begin_fill()
 drawing.color("red")
@@ -30,6 +32,7 @@ drawing.right(90)
 drawing.forward(150)
 drawing.end_fill()
 
+#Draws bottom triangle portion
 drawing.fillcolor("red")
 drawing.begin_fill()
 drawing.color("red")
@@ -38,6 +41,7 @@ drawing.right(90)
 drawing.forward(150)
 drawing.end_fill()
 
+#draws left side triangle portion
 drawing.fillcolor("red")
 drawing.begin_fill()
 drawing.color("red")
